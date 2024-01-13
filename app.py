001e4b10f8c8a51b1224d39ded8a26c86290bca1
2575044704 <2575044704@qq.com>
2024-01-13 21:24:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,11 @@ import os
 import subprocess
 
 def prepare():
-    # Update apt-get
-    subprocess.run(["apt-get", "update"])
-
-    # Install imjoy-elfinder
-    subprocess.run(["pip3", "install", "imjoy-elfinder"])
-
-    # Install aria2 and python3pip
-    print("Installing aria2 downloader...")
-    subprocess.run(["apt", "install", "-y", "aria2"])
-
     # Define an empty file path
     devnull = open(os.devnull, 'w')
 
     # Set standard output and standard error streams to redirect to an empty file
-    command = "imjoy-elfinder --root-dir=/kaggle --port=8076"
+    command = "imjoy-elfinder --root-dir=/ --port=7860"
     process = subprocess.Popen(command, shell=True, stdout=devnull, stderr=subprocess.STDOUT)
     devnull.close()
 
